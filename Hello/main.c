37a0684209da9f5057eee8d7e485c91eb6e96ea6
Mr. Braskin <abraskin.mchs.cs@gmail.com>
2014-07-01 10:29:29 -0700

    Some easy summing problems using for loops, simple variables, and various operators learned in Chapter 4 by page 59.

--- Hello/main.c
+++ Hello/main.c
@@ -8,11 +8,27 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[])
-{
+int main(int argc, const char * argv[]) {
 
     // insert code here...
-    printf("Hello, World!\n");
+    printf("Using for loops to sum ranges of numbers!\n");
+    int number, sum;
+    sum = 0;
+    for ( number = 1; number <= 10; number++ ) sum+=number;
+    printf(" The sum of the numbers from 1 to 10 is %d.\n", sum );
+    
+    sum = 0;
+    for ( number = 1; number <= 20; number+=2 ) sum+=number;
+    printf(" The sum of the odd numbers from 1 to 20 is %d.\n", sum );
+    
+    sum = 0;
+    for ( number = 2; number <= 10; number+=2 ) sum+=number;
+    printf(" The sum of the even numbers from 1 to 10 is %d.\n", sum );
+    
+    sum = 0;
+    for ( number = 10; number <= 100; number+=3 ) sum+=number;
+    printf(" The sum of every third number from 10 to 100 is %d.\n", sum );
+
     return 0;
 }
 
